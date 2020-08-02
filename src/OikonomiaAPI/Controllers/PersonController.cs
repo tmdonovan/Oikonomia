fb8f972a32ee7f9c782d40be7b41104221ce1147
TMDONOVAN <timma_d@hotmail.com>
2020-08-02 10:13:30 -0500

    Initial Commit

---
+++ src/OikonomiaAPI/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OikonomiaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>>Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
